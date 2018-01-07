c07d9aa3055448b4128418829911e6ab1c9f2207
Takeshi Ijima <takeshi.ijima.www@gmail.com>
2018-01-08 08:29:33 +0900

    ユーザー設定・データベースの作成など

---
+++ first-sql/lesson1/mysql.sql
@@ -0,0 +1,14 @@
+-- ログイン方法 ユーザー名とデータベース名を入力することでログインしてからuseしなくて済む
+-- mysql -u firstsql -p bank;
+
+-- 権限を与える
+-- grant all on bank.* to firstsql@localhost;
+
+-- データベースを使う
+-- use bank;
+
+-- データベースの作成
+-- create database bank;
+
+-- ユーザーの作成方法
+-- create user firstsql@localhost identified by 'hogehoge';
